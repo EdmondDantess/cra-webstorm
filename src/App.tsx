199702d18d0ba54122af6c21bc4549f1c4dec88a
Maksim Laurouski <maximlavrovsky@mail.ru>
2022-06-15 18:33:19 +0300

    commit-2

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <p>lesson 6</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <p>lesson 6</p>
+                <p>2</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
